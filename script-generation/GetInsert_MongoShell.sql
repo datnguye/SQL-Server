29699101e15737432fa30a98343db8999267f0dd
ssdavid <david@siliconstack.com.au>
2020-06-12 13:42:38 +0700

    adding some configs

--- script-generation/GetInsert_MongoShell.sql
+++ script-generation/GetInsert_MongoShell.sql
@@ -7,15 +7,19 @@
 --======================================================
 DROP PROCEDURE IF EXISTS [dbo].GetInsert_MongoShell
 GO
-CREATE PROCEDURE [dbo].GetInsert_MongoShell	@TableSchema sysname = 'dbo',
-											@TableName sysname,
+CREATE PROCEDURE [dbo].GetInsert_MongoShell	@TableName sysname,
+											@TableSchema sysname = 'dbo',
+											@Top INT = NULL,
 											@Where nvarchar(4000) = '',
-											@ExportPath nvarchar(255) = 'C:\Temp\'
+											@ExportPath nvarchar(255) = NULL,
+											@ExportOverrideServerInstance nvarchar(255) = NULL,
+											@OverrideDestinationCollection nvarchar(255) = NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
 
 	DECLARE @vCommand nvarchar(4000)
+	DECLARE @vCollectionName nvarchar(255) = COALESCE(@OverrideDestinationCollection, @TableName)
 	DECLARE @vReturnCode INT
 
 	SET @ExportPath += '<table-name>-<yyyyMMdd-HHmmss>.js' 
@@ -43,34 +47,39 @@ BEGIN
 	SET @vCommand = '
 	DECLARE @vJsonData nvarchar(max)
 	SELECT @vJsonData = (
-		SELECT		*
+		SELECT		TOP (@Top) *
 		FROM		<schema>.<table>
 		<where>
 		FOR JSON AUTO
 	)
-	INSERT INTO ##tJsonData SELECT ''db.createCollection("<table>");db.<table>.remove({});db.<table>.insert('' + @vJsonData + '')'''
+	INSERT INTO ##tJsonData SELECT ''db.createCollection("<collection>");db.<collection>.remove({});db.<collection>.insert('' + @vJsonData + '')'''
 
 	SET @vCommand = REPLACE(@vCommand, '<schema>', @TableSchema)
 	SET @vCommand = REPLACE(@vCommand, '<table>', @TableName)
+	SET @vCommand = REPLACE(@vCommand, '<collection>', @vCollectionName)
 	IF COALESCE(LTRIM(RTRIM(@Where)),'') <> ''
 		SET @Where = 'WHERE	' + @Where
 	ELSE 
 		SET @Where = ''
 	SET @vCommand = REPLACE(@vCommand, '<where>', @Where)
 
-	EXEC sp_executesql @vCommand
+	EXEC sp_executesql @vCommand, N'@Top INT', @Top
 
 	--Export to js file using sqlcmd
-	--SET @vCommand = 'sqlcmd -Q "SET NOCOUNT ON; SELECT * FROM ##tJsonData" -S ' + @@SERVERNAME + ' -E -h -1 -y 0 -o "' + @vExportPath + '"'
-	--EXEC @vReturnCode = xp_cmdshell @vCommand, no_output
-
-	SET @vCommand = 'bcp "SELECT InsertScript FROM ##tJsonData" QUERYOUT "' + @vExportPath + '" -T -w -S "' + @@SERVERNAME+ '"'
-	RAISERROR(@vCommand,0,1) WITH NOWAIT
-	EXEC @vReturnCode = xp_cmdshell @vCommand, no_output
-	IF @vReturnCode <> 0
+	IF @ExportPath IS NOT NULL
 	BEGIN
-		RAISERROR('Exporting has been failed',16,1) WITH NOWAIT
-		RETURN -1
+		SET @vCommand = 'bcp "SELECT InsertScript FROM ##tJsonData" QUERYOUT "' + @vExportPath + '" -T -w -S "' + COALESCE(@ExportOverrideServerInstance, @@SERVERNAME) + '"'
+		RAISERROR(@vCommand,0,1) WITH NOWAIT
+		EXEC @vReturnCode = xp_cmdshell @vCommand, no_output
+		IF @vReturnCode <> 0
+		BEGIN
+			RAISERROR('Exporting has been failed',16,1) WITH NOWAIT
+			RETURN -1
+		END
+	END
+	ELSE 
+	BEGIN
+		SELECT InsertScript FROM ##tJsonData
 	END
 
 	RETURN
@@ -78,6 +87,11 @@ END
 GO
 /*
 --This sample will get results of https://github.com/datnguye/SQL-Server/blob/master/web-call/ApiCovid19.sql to generate mongo shell script
+
+EXEC GetInsert_MongoShell	@TableName='ApiCovid19Route'
+EXEC GetInsert_MongoShell	@TableName='ApiCovid19CountryDayOne', @Top = 10, @Where = 'CountryCode = ''vn'''
+EXEC GetInsert_MongoShell	@TableName='ApiCovid19Route'
+
 EXEC GetInsert_MongoShell	@TableName='ApiCovid19Route', @ExportPath = 'C:\Temp\'
 EXEC GetInsert_MongoShell	@TableName='ApiCovid19Countries', @ExportPath = 'C:\Temp\'
 EXEC GetInsert_MongoShell	@TableName='ApiCovid19CountryDayOne', @ExportPath = 'C:\Temp\'
