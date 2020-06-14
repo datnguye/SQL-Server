02085041ba6987a2bfcfa329211d35a5b473a364
datnguye <datnguyen.it09@gmail.com>
2020-06-14 12:54:59 +0700

    no message

---
+++ ftp/FtpDownload-CMD.sql
@@ -0,0 +1,91 @@
+--======================================================
+-- Usage: FtpDownload
+-- Notes: xp_cmdshell needs enabling
+--		EXEC sp_configure 'xp_cmdshell', 1
+--		RECONFIGURE;
+-- Parameters:
+-- History:
+-- Date			Author		Description
+-- 2020-06-14	DN			Intial
+--======================================================
+DROP PROCEDURE IF EXISTS FtpDownload
+GO
+CREATE PROCEDURE [dbo].[FtpDownload]	@FtpFolder nvarchar(255) = '/',
+										@FtpFileName nvarchar(255),
+										@FtpHost nvarchar(255),
+										@FtpUser nvarchar(255),
+										@FtpPassword nvarchar(255),
+										@LocalFolder nvarchar(255) = 'C:\Temp\'
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @vMessage nvarchar(MAX)
+	DECLARE @vCommand nvarchar(4000)
+	DECLARE @RunFileName nvarchar(255) = 'FtpDownload_RUN.txt'
+	DECLARE @vReturnCode INT = 0
+
+	IF RIGHT(@FtpFolder,1) <> '/' SET @FtpFolder += '/'
+	IF RIGHT(@LocalFolder,1) <> '\' AND RIGHT(@LocalFolder,1) <> '/' SET @LocalFolder += '\'
+	SET @RunFileName = @LocalFolder + @RunFileName
+
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'	Starting FTP download files from: ' + @FtpHost + ' - with user: ' + @FtpUser
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Remote folder: ' + @FtpFolder
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Local folder: ' + @LocalFolder
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		File: ' + @FtpFileName
+	RAISERROR(@vMessage,0,1)
+	
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Creating run file under: ' + @RunFileName
+	RAISERROR(@vMessage,0,1)
+	SET @vCommand = 'echo open ' + @FtpHost + ' > ' + @RunFileName
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+
+	SET @vCommand = 'echo ' + @FtpUser + '>> "' + @RunFileName + '"'
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+
+	SET @vCommand = 'echo ' + @FtpPassword + '>> "' + @RunFileName + '"'
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+
+	SET @vCommand = 'echo get "' + @FtpFolder + @FtpFileName + '" "' + @LocalFolder + @FtpFileName + '" >> "' + @RunFileName + '"'
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+	
+	SET @vCommand = 'echo quit >> "' + @RunFileName + '"'
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+	
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Start running'
+	RAISERROR(@vMessage,0,1)
+	SET @vCommand = 'ftp -s:"' + @RunFileName + '"'
+	RAISERROR(@vCommand,0,1)
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand
+	IF @vReturnCode <> 0 GOTO EXCEPTION
+	
+	IF @vReturnCode = 0 GOTO DONE
+	EXCEPTION:
+	BEGIN
+		SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Something wrong happened'
+		RAISERROR(@vMessage,16,1)
+
+		RETURN -1
+	END
+	
+	DONE:
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'	Finished'
+	RAISERROR(@vMessage,0,1)
+
+	RETURN
+END
+GO
+/*
+	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser',
+							@FtpFolder = '/', @LocalFolder = 'C:\Temp',
+							@FtpFileName = 'Countries-20200614-090721.sql'
+*/
+
+
---
+++ ftp/FtpDownload-Python.sql
@@ -0,0 +1,83 @@
+--======================================================
+-- Usage: FtpDownload
+-- Notes: xp_cmdshell needs enabling
+--		EXEC sp_configure 'xp_cmdshell', 1
+--		RECONFIGURE;
+-- Parameters:
+-- History:
+-- Date			Author		Description
+-- 2020-06-14	DN			Intial
+--======================================================
+DROP PROCEDURE IF EXISTS FtpDownload
+GO
+CREATE PROCEDURE [dbo].[FtpDownload]	@FtpFolder nvarchar(255) = '/',
+										@FtpFileMask nvarchar(255) = '*',
+										@FtpHost nvarchar(255),
+										@FtpUser nvarchar(255),
+										@FtpPassword nvarchar(255),
+										@LocalFolder nvarchar(255) = 'C:\Temp\',
+										@WinSCPFolder nvarchar(255) = 'C:\Program Files (x86)\WinSCP\',
+										@LogFolder nvarchar(255) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @vMessage nvarchar(MAX)
+	DECLARE @vCommand nvarchar(4000)
+	DECLARE @LogFileName nvarchar(255) = 'FtpDownload_log_' + FORMAT(GETDATE(),'_yyyyMMdd_HHmmss') + '.log'
+	DECLARE @vReturnCode INT = 0
+
+	IF RIGHT(@FtpFolder,1) <> '/' SET @FtpFolder += '/'
+	IF RIGHT(@LocalFolder,1) <> '\' AND RIGHT(@LocalFolder,1) <> '/' SET @LocalFolder += '\'
+	IF @LogFolder IS NULL SET @LogFolder = @LocalFolder
+
+	SET @LogFileName = @LocalFolder + @LogFileName
+
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'	Starting FTP download files from: ' + @FtpHost + ' - with user: ' + @FtpUser
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Remote folder: ' + @FtpFolder
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Local folder: ' + @LocalFolder
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		File(s): ' + @FtpFileMask
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Log: ' + @LogFileName
+	RAISERROR(@vMessage,0,1)
+
+	SET @vCommand = LEFT(@LocalFolder,2) + 
+					' & cd "' + @LocalFolder + '"' + 
+					' & "' + @WinSCPFolder + 'WinSCP.com" /log="' + @LogFileName + '" /command ' + 
+					'"open ftp://' + @FtpUser + ':' + @FtpPassword + '@' + @FtpHost + '" ' + 
+					'"get ""' + @FtpFolder + '*"" -filemask=' + @FtpFileMask + '" "exit"'
+
+	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+	
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Command used: '
+	RAISERROR(@vMessage,0,1)
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'			' + @vCommand
+	RAISERROR(@vMessage,0,1)
+	IF @vReturnCode <> 0
+	BEGIN
+		SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Something wrong happened. Please help to review log: ' + @LogFileName
+		RAISERROR(@vMessage,16,1)
+
+		RETURN -1
+	END
+	
+	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'	Finished'
+	RAISERROR(@vMessage,0,1)
+
+	RETURN
+END
+GO
+/*
+	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser',
+							@FtpFolder = '/', @LocalFolder = 'C:\Temp'
+
+	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser', @FtpFileMask = '*-20200614-090721.sql',
+							@FtpFolder = '/', @LocalFolder = 'C:\Temp'
+
+	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser', @FtpFileMask = 'Countries-20200614-090721.sql',
+							@FtpFolder = '/', @LocalFolder = 'C:\Temp'
+*/
+
+
