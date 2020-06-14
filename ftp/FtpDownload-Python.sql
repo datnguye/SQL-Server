7f7a934c403eb4801556c8c306d813916920f080
datnguye <datnguyen.it09@gmail.com>
2020-06-14 16:18:41 +0700

    update

--- ftp/FtpDownload-Python.sql
+++ ftp/FtpDownload-Python.sql
@@ -13,22 +13,16 @@ CREATE PROCEDURE [dbo].[FtpDownload]	@FtpFolder nvarchar(255) = '/',
 										@FtpHost nvarchar(255),
 										@FtpUser nvarchar(255),
 										@FtpPassword nvarchar(255),
-										@LocalFolder nvarchar(255) = 'C:\Temp\',
-										@WinSCPFolder nvarchar(255) = 'C:\Program Files (x86)\WinSCP\',
-										@LogFolder nvarchar(255) = NULL
+										@LocalFolder nvarchar(255) = 'C:\Temp\'
 AS
 BEGIN
 	SET NOCOUNT ON
 	DECLARE @vMessage nvarchar(MAX)
 	DECLARE @vCommand nvarchar(4000)
-	DECLARE @LogFileName nvarchar(255) = 'FtpDownload_log_' + FORMAT(GETDATE(),'_yyyyMMdd_HHmmss') + '.log'
 	DECLARE @vReturnCode INT = 0
 
 	IF RIGHT(@FtpFolder,1) <> '/' SET @FtpFolder += '/'
 	IF RIGHT(@LocalFolder,1) <> '\' AND RIGHT(@LocalFolder,1) <> '/' SET @LocalFolder += '\'
-	IF @LogFolder IS NULL SET @LogFolder = @LocalFolder
-
-	SET @LogFileName = @LocalFolder + @LogFileName
 
 	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'	Starting FTP download files from: ' + @FtpHost + ' - with user: ' + @FtpUser
 	RAISERROR(@vMessage,0,1)
@@ -38,16 +32,37 @@ BEGIN
 	RAISERROR(@vMessage,0,1)
 	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		File(s): ' + @FtpFileMask
 	RAISERROR(@vMessage,0,1)
-	SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Log: ' + @LogFileName
-	RAISERROR(@vMessage,0,1)
 
-	SET @vCommand = 'TO BE UPDATED'
+	SET @vCommand = N'
+from ftplib import FTP
+import fnmatch as fm
 
-	EXEC @vReturnCode = master..xp_cmdshell @vCommand, no_output
+ftp = FTP()
+
+ftp.connect(host="' + @FtpHost + N'")
+ftp.login(user="' + @FtpUser + N'", passwd="' + @FtpPassword + N'")
+
+ftp.cwd("' + @FtpFolder + N'")
+
+files = ftp.nlst()
+files = (file for file in files if fm.fnmatch(file, "' + @FtpFileMask + N'"))
+
+for file in files:
+    print(f"Downloading {file}")
+    with open("' + REPLACE(@LocalFolder,'\','\\') + N'" + file, "wb") as fp:
+        ftp.retrbinary("RETR " + file, fp.write)
+
+ftp.quit()
+print("Done")'
+
+	--PRINT @vCommand
+	EXEC @vReturnCode = sp_execute_external_script 
+		@language =N'Python', 
+		@script= @vCommand
 
 	IF @vReturnCode <> 0
 	BEGIN
-		SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Something wrong happened. Please help to review log: ' + @LogFileName
+		SET @vMessage = CONVERT(nvarchar,CURRENT_TIMESTAMP,21)+'		Something wrong happened. Please help to review message log'
 		RAISERROR(@vMessage,16,1)
 
 		RETURN -1
@@ -60,8 +75,8 @@ BEGIN
 END
 GO
 /*
-	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser',
-							@FtpFolder = '/', @LocalFolder = 'C:\Temp'
+	EXEC dbo.FtpDownload	@FtpHost='localhost', @FtpUser='ftpuser', @FtpPassword='ftpuser', @FtpFolder = '/', 
+							@LocalFolder = 'C:\Temp'
 */
 
 
